85f9e772e2ffe847606581d49fbe741e1cb98831
8252_dtg (Teacher) <julien.hamaide@gmail.com>
2016-05-19 13:57:36 +0200

    Extract tripple weapon

---
+++ ShooterTutorial/GameObjects/TripleWeapon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace ShooterTutorial.GameObjects
+{
+    class TripleWeapon : Weapon
+    {
+        public TripleWeapon(Game1 game, Player player) : base(game, player)
+        {
+        }
+
+        public override void Fire(GameTime gameTime)
+        {
+            // govern the rate of fire for our lasers
+            if (gameTime.TotalGameTime - _previousLaserSpawnTime > _laserSpawnTime)
+            {
+                _previousLaserSpawnTime = gameTime.TotalGameTime;
+
+                // Add the laer to our list.
+                _game.AddLaser(LinearMovement.create(_player.Position, -10f, -30.0f * (float)Math.PI / 180.0f));
+                _game.AddLaser(LinearMovement.create(_player.Position, 0f, 0.0f));
+                _game.AddLaser(LinearMovement.create(_player.Position, 10f, 30.0f * (float)Math.PI / 180.0f));
+            }
+
+        }
+    }
+}
