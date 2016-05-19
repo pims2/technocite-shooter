3809ce95140d3f6d81e6a3ed7b92a8ddef5dad89
Haye Gregory <gha.dev@outlook.com>
2016-05-19 16:08:11 +0200

    ajout ghamovement

--- ShooterTutorial/GameObjects/ghaMovement.cs
+++ ShooterTutorial/GameObjects/ghaMovement.cs
@@ -17,24 +17,40 @@ namespace ShooterTutorial.GameObjects
         public Vector2 Direction;
 
         // the speed the laser traves
-        float laserMoveSpeed = 30f;
+        float laserMoveSpeed = 20f;
         // rotation
         float Rotation = 0f;
-
-        
+        double cpt;
+        bool etat;
 
         public ghaMovement(Vector2 position, Vector2 direction, float rotation)
         {
             Position = position;
             Rotation = rotation;
             Direction = direction;
+            cpt = 0;
+            etat = true;
             Direction.Normalize();
         }
 
         public void update( GameTime gameTime )
         {
-            Position += laserMoveSpeed * Direction;
+            cpt++;
+            Rotation+=(float)gameTime.ElapsedGameTime.TotalSeconds-4.5f;
             
+            if (etat)
+            {
+                Position += laserMoveSpeed-- * Direction;
+                etat = laserMoveSpeed < 0 ? !etat : etat;
+            }
+            else
+            {
+                if (cpt > 50)
+                {
+                    Position += (laserMoveSpeed + 4 * 4.50f) * Direction;
+                    Direction.X+=0.01f;
+                }
+            }
         }
 
         public Vector2 getPosition()
@@ -44,7 +60,7 @@ namespace ShooterTutorial.GameObjects
 
         public float getRotation()
         {
-            return Rotation;
+            return Rotation ;
         }
 
         public static ghaMovement create(Vector2 position, float verticalOffset, float angle, float rotation)
@@ -52,11 +68,11 @@ namespace ShooterTutorial.GameObjects
             // Adjust the position slightly to match the muzzle of the cannon.
             position.Y += 37 + verticalOffset;
             position.X += 70;
-
+            
             var direction = new Vector2();
 
-            direction.X = (float)Math.Cos((double)angle);
-            direction.Y = (float)Math.Sin((double)angle);
+            direction.X = (float)Math.Cos((double)angle) ;
+            direction.Y = (float)Math.Sin((double)angle) ;
 
             return new ghaMovement(position, direction, rotation);
         }
