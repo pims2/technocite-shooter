72900290ddc18889f6eff987d47e934643fa4ef4
8252_dtg (Teacher) <julien.hamaide@gmail.com>
2016-05-19 12:16:19 +0200

    Missing files

---
+++ ShooterTutorial/GameObjects/IMovement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace ShooterTutorial.GameObjects
+{
+    public interface IMovement
+    {
+        void update(GameTime time);
+
+        Vector2 getPosition();
+        float getRotation();
+    }
+}
---
+++ ShooterTutorial/GameObjects/LinearMovement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace ShooterTutorial.GameObjects
+{
+    class LinearMovement: IMovement
+    {
+        // postion of the laser
+        public Vector2 Position;
+
+        // Direction of the laser
+        public Vector2 Direction;
+
+        // the speed the laser traves
+        float laserMoveSpeed = 30f;
+
+
+        public LinearMovement(Vector2 position, Vector2 direction)
+        {
+            Position = position;
+
+            Direction = direction;
+            Direction.Normalize();
+        }
+
+        public void update( GameTime gameTime )
+        {
+            Position += laserMoveSpeed * Direction;
+        }
+
+        public Vector2 getPosition()
+        {
+            return Position;
+        }
+
+        public float getRotation()
+        {
+            return 0f;
+        }
+
+        public static LinearMovement create(Vector2 position, float verticalOffset, float angle)
+        {
+            // Adjust the position slightly to match the muzzle of the cannon.
+            position.Y += 37 + verticalOffset;
+            position.X += 70;
+
+            var direction = new Vector2();
+
+            direction.X = (float)Math.Cos((double)angle);
+            direction.Y = (float)Math.Sin((double)angle);
+
+            return new LinearMovement(position, direction);
+        }
+    }
+}
