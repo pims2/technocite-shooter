d53ac35fbd3384352318cc08d55f148207f81f65
8252_dtg (Teacher) <julien.hamaide@gmail.com>
2016-05-30 16:15:24 +0200

    state machine base

---
+++ ShooterTutorial/GameObjects/StateMachine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace ShooterTutorial.GameObjects
+{
+    class StateMachine
+    {
+        private State _state;
+
+        public StateMachine(State state)
+        {
+            _state = state;
+            _state.OnEnter();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _state.OnUpdate(gameTime);
+        }
+
+        public void ChangeState(State state)
+        {
+            // :TODO: check if changing to same state
+
+            _state.OnExit();
+            _state = state;
+            _state.OnEnter();
+        }
+    }
+}
