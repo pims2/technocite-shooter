7c96fe240f7d06beb61b646c2c379ece3a894ea2
8252_dtg (Teacher) <julien.hamaide@gmail.com>
2016-06-02 12:07:11 +0200

    Missing file

---
+++ ShooterTutorial/Configuration/ConfigurationTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShooterTutorial.Configuration
+{
+    class ConfigurationTable<T> : ConfigurationValueBase
+    {
+        List<T> ValueTable = new List<T>();
+
+        public ConfigurationTable( string name, string description) : base(name, description )
+        {
+            
+        }
+
+        public static implicit operator List<T>( ConfigurationTable<T> value )
+        {
+            return value.ValueTable;
+        }
+
+        public void Clear()
+        {
+            ValueTable.Clear();
+        }
+
+        public void Add(T value)
+        {
+            ValueTable.Add(value);
+        }
+    }
+}
